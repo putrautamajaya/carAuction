30e59227ee4fa1a5cc9747ce95f94cdc4c81c9d4
Putra Utama Jaya <putra.utamajaya@ematicsolutions.com>
2022-06-09 14:34:54 +0700

    top navigation 1.0 - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule,
