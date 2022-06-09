fee12709c9a13f259b4809b3d34200bff7fa34ab
Putra Utama Jaya <putra.utamajaya@ematicsolutions.com>
2022-06-09 16:55:57 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
     "./src/app/app.component.html",
     "./src/app/pages/home/home.component.html",
-    "./src/app/pages/login/login.component.html"
+    "./src/app/pages/login/login.component.html",
+    "./src/app/components/top-navigation/top-navigation.component.html"
   ],
   theme: {
     extend: {},
