e3f4a2a2f7ada4dda7e215d9019f5ab1d22f7455
Putra Utama Jaya <putra.utamajaya@ematicsolutions.com>
2022-06-10 13:49:05 +0700

    dropdown a 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     "./src/app/app.component.html",
     "./src/app/pages/home/home.component.html",
     "./src/app/pages/login/login.component.html",
-    "./src/app/components/top-navigation/top-navigation.component.html"
+    "./src/app/components/top-navigation/top-navigation.component.html",
+    "./src/app/components/dropdown-a/dropdown-a.component.html"
   ],
   theme: {
     extend: {},
